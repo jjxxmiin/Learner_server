36b5f3cbc4d8843ec986ecc7faea3baf9c79b744
jjeamin <woalsdl500@naver.com>
2020-08-24 14:18:53 +0900

    Update API
    - Update train
    - Update infer

--- api.py
+++ api.py
@@ -1,16 +1,39 @@
+import os
 import flask
 import werkzeug
+import requests
+from pathlib import Path
+
+datasets_path = Path("train_datasets")
 
 app = flask.Flask(__name__)
 
 
-@app.route('/', methods = ['GET', 'POST'])
-def handle_request():
+@app.route('/train', methods = ['GET', 'POST'])
+def handle_train():
     imagefile = flask.request.files['image']
-    filename = werkzeug.utils.secure_filename(imagefile.filename)
-    print("\nReceived image File name : " + imagefile.filename)
-    imagefile.save(filename)
+    label = flask.request.form['label']
+
+    filename = imagefile.filename
+
+    # filename = werkzeug.utils.secure_filename(imagefile.filename)
+    print("\nReceived image File name : " + filename)
+
+    save_path = datasets_path / label
+
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    imagefile.save(save_path / filename)
+
     return "Image Uploaded Successfully"
+
+
+@app.route('/infer', methods = ['GET', 'POST'])
+def handle_infer():
+    imagefile = flask.request.files['image']
+
+    return "정재민"
 
 
 app.run(host="0.0.0.0", port=5000, debug=True)
